3a9c1ed96c1e05eaeb6ff93236d40e8360642ab4
Santi Diaz <sdiaz@aidacanarias.com>
2021-11-11 06:36:40 +0000

    Extract methods from GetScore

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,71 +2,93 @@ namespace Tennis
 {
     class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private int _mScore1 = 0;
+        private int _mScore2 = 0;
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                _mScore1 += 1;
             else
-                m_score2 += 1;
+                _mScore2 += 1;
         }
 
         public string GetScore()
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (_mScore1 == _mScore2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = HandleEqualScore();
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (_mScore1 >= 4 || _mScore2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = HandleAdvantageOrWinScore();
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    if (i == 1) tempScore = _mScore1;
+                    else { score += "-"; tempScore = _mScore2; }
+
+                    score = HandleGeneralScore(tempScore, score);
                 }
             }
+            return score;
+        }
+
+        private static string HandleGeneralScore(int tempScore, string score)
+        {
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+
+            return score;
+        }
+
+        private string HandleAdvantageOrWinScore()
+        {
+            string score;
+            var minusResult = _mScore1 - _mScore2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private string HandleEqualScore()
+        {
+            string score;
+            switch (_mScore1)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+
             return score;
         }
     }
